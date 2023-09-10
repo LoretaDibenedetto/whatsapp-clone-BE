6a6fdd3d0543b775dc05bc6c688a736ae4808875
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-09-10 14:59:01 +0200

    add schema

--- server.mjs
+++ server.mjs
@@ -1,8 +1,12 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import messages from  "./models/dbMessages.mjs";
 
 const app = express();
 const port = process.env.PORT || 9000;
+
+//database
+const connectionDbURl = "";
 
 app.get('/api',(req,res) => {
     res.status(200).send("Benvenuto sul server");
