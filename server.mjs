14e0aa99522cac14ef251cb1dcd2d0a1b8b4a5bb
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-09-10 15:38:16 +0200

    add string connection

--- server.mjs
+++ server.mjs
@@ -6,7 +6,18 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 //database
-const connectionDbURl = "";
+const connectionDbURl = "mongodb+srv://admin:RDeL4qWKhlh4Q8S5@cluster0.vq2xgak.mongodb.net/whatsappwebdb?retryWrites=true&w=majority";
+//admin
+//3!urKGR@U.E@RG2
+mongoose.connect(connectionDbURl,
+    {
+        useCreateIndex : true,
+        useNewUrlParser : true,
+        useUnifiedTopology: true
+    },
+(err)=>{
+    if(!err) console.log("mongoDB connect");
+});
 
 app.get('/api',(req,res) => {
     res.status(200).send("Benvenuto sul server");
