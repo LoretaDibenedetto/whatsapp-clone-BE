39a26cc61c553d2d3aebebb39f48de6b38bf5ae9
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-09-10 14:28:08 +0200

    prepare server

---
+++ server.mjs
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.get('/api',(req,res) => {
+    res.status(200).send("Benvenuto sul server");
+});
+
+app.listen(port,()=>{
+    console.log('server start on port: ${port}');
+});
